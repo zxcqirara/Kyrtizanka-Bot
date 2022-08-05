ff3b03d6f6916bb7bfe307a0ea35a09b15577516
ChA0S-f4me <Nikita.da.ok@gmail.com>
2022-08-05 17:32:35 +0300

    Added default value to mem row

--- src/main/kotlin/bot/database/meme/Memes.kt
+++ src/main/kotlin/bot/database/meme/Memes.kt
@@ -3,5 +3,5 @@ package bot.database.meme
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object Memes : LongIdTable(columnName = "message_id") {
-	val maxScore = integer("max_score")
+	val maxScore = integer("max_score").default(0)
 }
