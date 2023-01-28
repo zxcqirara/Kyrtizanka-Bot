2518e95f1ac926467d16771bb3c09f361314a174
ChA0S-f4me <Nikita.da.ok@gmail.com>
2023-01-29 01:49:52 +0300

    Added stats view by user + some code refactoring

--- src/main/kotlin/bot/extensions/Statistic.kt
+++ src/main/kotlin/bot/extensions/Statistic.kt
@@ -1,11 +1,17 @@
 package bot.extensions
 
 import bot.database.experience.Experience
+import bot.database.experience.Experiences
 import bot.lib.ChartGithubTheme
 import bot.lib.Config
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.application.slash.PublicSlashCommandContext
 import com.kotlindiscord.kord.extensions.commands.application.slash.publicSubCommand
+import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalMember
+import com.kotlindiscord.kord.extensions.components.forms.ModalForm
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
+import com.kotlindiscord.kord.extensions.i18n.TranslationsProvider
 import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.core.behavior.interaction.followup.edit
 import dev.kord.rest.NamedFile
@@ -17,6 +23,7 @@ import org.knowm.xchart.BitmapEncoder
 import org.knowm.xchart.BitmapEncoder.BitmapFormat
 import org.knowm.xchart.XYChartBuilder
 import org.knowm.xchart.style.markers.SeriesMarkers
+import org.koin.core.component.inject
 import java.awt.BasicStroke
 import java.awt.Color
 import java.awt.Font
@@ -27,8 +34,10 @@ class Statistic : Extension() {
 	override val name = "visual-stats"
 	override val bundle = "cs_dsbot"
 
+	private val translationsProvider: TranslationsProvider by inject()
+	private val timeZone = TimeZone.of(Config.discord.timeZone)
+
 	override suspend fun setup() {
-		val timeZone = TimeZone.of(Config.discord.timeZone)
 
 		publicSlashCommand {
 			name = "extensions.statistic.commandName"
@@ -39,91 +48,123 @@ class Statistic : Extension() {
 				description = "extensions.statistic.global.commandDescription"
 
 				action {
-					val gray = Color(0x868E96)
-					val green = Color(0x40C057)
-					val red = Color(0xF03E3E)
-
 					val dbData = transaction {
-						Experience.all().sortedBy { it.time }.toList()
+						Experience
+							.all()
+							.sortedBy { it.time }
+							.toList()
 					}
 
-					val data = dbData.groupBy { it.time.toLocalDateTime(timeZone).dayOfYear }
-
-					val xData = data.map { it.key }
-					val yData = data.map { it.value }.map { value -> value.map { it.count }.sum() }
-
-					val chart = XYChartBuilder()
-						.xAxisTitle("День месяца")
-						.yAxisTitle("Кол-во опыта")
-						.width(400)
-						.height(225)
-						.build()
-
-					chart.styler.theme = ChartGithubTheme()
-					chart.styler.axisTitleFont = Font(Font.DIALOG, Font.PLAIN, 10)
-					chart.styler.axisTickLabelsFont = Font(Font.DIALOG, Font.PLAIN, 8)
-
-					yData.forEachIndexed { index, yCurrent ->
-						val xCurrent = xData[index]
-						val xNext = xData.getOrNull(index + 1) ?: return@forEachIndexed
-						val yNext = yData.getOrNull(index + 1) ?: return@forEachIndexed
-
-						val lineSeries = chart.addSeries(
-							"line-$index",
-							listOf(xCurrent, xNext),
-							listOf(yCurrent, yNext)
-						)
-
-						lineSeries.lineColor = gray
-						lineSeries.lineStyle = BasicStroke(1f)
-						lineSeries.marker = SeriesMarkers.NONE
-					}
-
-					yData.forEachIndexed { index, yCurrent ->
-						val xNext = xData.getOrNull(index + 1) ?: return@forEachIndexed
-						val yNext = yData.getOrNull(index + 1) ?: return@forEachIndexed
-
-						val markerSeries = chart.addSeries(
-							"marker-$index",
-							listOf(xNext),
-							listOf(yNext)
-						)
-
-						markerSeries.lineColor = gray
-						markerSeries.lineStyle = BasicStroke(1f)
-
-						println("$yCurrent > $yNext")
-						if (yNext > yCurrent) {
-							markerSeries.marker = SeriesMarkers.TRIANGLE_UP
-							markerSeries.markerColor = green
-						}
-						else if (yNext < yCurrent) {
-							markerSeries.marker = SeriesMarkers.TRIANGLE_DOWN
-							markerSeries.markerColor = red
-						}
-						else {
-							markerSeries.marker = SeriesMarkers.DIAMOND
-						}
-					}
-
-					val path = "${event.interaction.id.value}.png"
-
-					BitmapEncoder.saveBitmapWithDPI(chart, path, BitmapFormat.PNG, 300)
-
-					lateinit var chartFile: NamedFile
-					val message = respond { chartFile = addFile(Path(path)) }
-
-					File(path).delete()
-
-					message.edit {
-						embed {
-							title = "Статистика опыта"
-
-							image = chartFile.url
-						}
-					}
+					procedureGraphSend(this, dbData)
 				}
 			}
+
+			publicSubCommand(::TargetArgs) {
+				name = "extensions.statistic.user.commandName"
+				description = "extensions.statistic.user.commandDescription"
+
+				action {
+					val targetId = (arguments.target ?: member!!).id.value.toLong()
+
+					val dbData = transaction {
+						Experience
+							.find { Experiences.userId eq targetId }
+							.sortedBy { it.time }
+							.toList()
+					}
+
+					procedureGraphSend(this, dbData)
+				}
+			}
+		}
+	}
+
+	private suspend fun procedureGraphSend(context: PublicSlashCommandContext<*, ModalForm>, dbData: List<Experience>) {
+		val gray = Color(0x868E96)
+		val green = Color(0x40C057)
+		val red = Color(0xF03E3E)
+
+		val data = dbData.groupBy { it.time.toLocalDateTime(timeZone).dayOfYear }
+
+		val xData = data.map { it.key }
+		val yData = data.map { it.value }.map { value -> value.map { it.count }.sum() }
+
+		val chart = XYChartBuilder()
+			.xAxisTitle(translationsProvider.translate("extensions.statistic.embed.image.dayOfMonth"))
+			.yAxisTitle(translationsProvider.translate("extensions.statistic.embed.image.experienceCount"))
+			.width(400)
+			.height(225)
+			.build()
+
+		chart.styler.theme = ChartGithubTheme()
+		chart.styler.axisTitleFont = Font(Font.DIALOG, Font.PLAIN, 10)
+		chart.styler.axisTickLabelsFont = Font(Font.DIALOG, Font.PLAIN, 8)
+
+		yData.forEachIndexed { index, yCurrent ->
+			val xCurrent = xData[index]
+			val xNext = xData.getOrNull(index + 1) ?: return@forEachIndexed
+			val yNext = yData.getOrNull(index + 1) ?: return@forEachIndexed
+
+			val lineSeries = chart.addSeries(
+				"line-$index",
+				listOf(xCurrent, xNext),
+				listOf(yCurrent, yNext)
+			)
+
+			lineSeries.lineColor = gray
+			lineSeries.lineStyle = BasicStroke(1f)
+			lineSeries.marker = SeriesMarkers.NONE
+		}
+
+		yData.forEachIndexed { index, yCurrent ->
+			val xNext = xData.getOrNull(index + 1) ?: return@forEachIndexed
+			val yNext = yData.getOrNull(index + 1) ?: return@forEachIndexed
+
+			val markerSeries = chart.addSeries(
+				"marker-$index",
+				listOf(xNext),
+				listOf(yNext)
+			)
+
+			markerSeries.lineColor = gray
+			markerSeries.lineStyle = BasicStroke(1f)
+
+			println("$yCurrent > $yNext")
+			if (yNext > yCurrent) {
+				markerSeries.marker = SeriesMarkers.TRIANGLE_UP
+				markerSeries.markerColor = green
+			}
+			else if (yNext < yCurrent) {
+				markerSeries.marker = SeriesMarkers.TRIANGLE_DOWN
+				markerSeries.markerColor = red
+			}
+			else {
+				markerSeries.marker = SeriesMarkers.DIAMOND
+			}
+		}
+
+		val path = "${context.event.interaction.id.value}.png"
+
+		BitmapEncoder.saveBitmapWithDPI(chart, path, BitmapFormat.PNG, 300)
+
+		lateinit var chartFile: NamedFile
+		val message = context.respond { chartFile = addFile(Path(path)) }
+
+		File(path).delete()
+
+		message.edit {
+			embed {
+				title = translationsProvider.translate("extensions.statistic.embed.title")
+
+				image = chartFile.url
+			}
+		}
+	}
+
+	inner class TargetArgs : Arguments() {
+		val target by optionalMember {
+			name = "user"
+			description = translationsProvider.translate("extensions.statistic.user.arguments.target")
 		}
 	}
 }
