81ee585c8e15525e7ab329557055221fefd625f1
ChA0S-f4me <Nikita.da.ok@gmail.com>
2023-02-14 17:17:10 +0300

    Added filter by time for statistic

--- src/main/kotlin/bot/extensions/Statistic.kt
+++ src/main/kotlin/bot/extensions/Statistic.kt
@@ -7,6 +7,7 @@ import bot.lib.Config
 import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.application.slash.PublicSlashCommandContext
 import com.kotlindiscord.kord.extensions.commands.application.slash.converters.ChoiceEnum
+import com.kotlindiscord.kord.extensions.commands.application.slash.converters.impl.defaultingEnumChoice
 import com.kotlindiscord.kord.extensions.commands.application.slash.publicSubCommand
 import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalMember
 import com.kotlindiscord.kord.extensions.components.forms.ModalForm
@@ -17,6 +18,7 @@ import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.rest.builder.message.create.embed
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.knowm.xchart.BitmapEncoder
 import org.knowm.xchart.BitmapEncoder.BitmapFormat
@@ -28,6 +30,8 @@ import java.awt.Color
 import java.awt.Font
 import java.io.File
 import kotlin.io.path.Path
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
 
 class Statistic : Extension() {
 	override val name = "visual-stats"
@@ -42,19 +46,26 @@ class Statistic : Extension() {
 			name = "extensions.statistic.commandName"
 			description = "extensions.statistic.commandDescription"
 
-			publicSubCommand {
+			publicSubCommand(::TimeArgs) {
 				name = "extensions.statistic.global.commandName"
 				description = "extensions.statistic.global.commandDescription"
 
 				action {
+					val duration = when (arguments.time) {
+						TimeChoice.HOUR -> 1.hours
+						TimeChoice.DAY -> 1.days
+						TimeChoice.MONTH -> 32.days
+					}
+					val relative = event.interaction.data.id.timestamp + duration
+
 					val dbData = transaction {
 						Experience
-							.all()
+							.find { Experiences.time lessEq relative }
 							.sortedBy { it.time }
 							.toList()
 					}
 
-					procedureGraphSend(this, dbData)
+					procedureGraphSend(this, dbData, arguments.time)
 				}
 			}
 
@@ -64,29 +75,43 @@ class Statistic : Extension() {
 
 				action {
 					val targetId = (arguments.target ?: member!!).id.value.toLong()
+					val duration = when (arguments.time) {
+						TimeChoice.HOUR -> 1.hours
+						TimeChoice.DAY -> 1.days
+						TimeChoice.MONTH -> 32.days
+					}
+					val relative = event.interaction.data.id.timestamp + duration
 
 					val dbData = transaction {
 						Experience
-							.find { Experiences.userId eq targetId }
+							.find { (Experiences.userId eq targetId) and (Experiences.time lessEq relative) }
 							.sortedBy { it.time }
 							.toList()
 					}
 
-					procedureGraphSend(this, dbData)
+					procedureGraphSend(this, dbData, arguments.time)
 				}
 			}
 		}
 	}
 
-	private suspend fun procedureGraphSend(context: PublicSlashCommandContext<*, ModalForm>, dbData: List<Experience>) {
+	private suspend fun procedureGraphSend(context: PublicSlashCommandContext<*, ModalForm>, dbData: List<Experience>, timeChoice: TimeChoice) {
 		val gray = Color(0x868E96)
 		val green = Color(0x40C057)
 		val red = Color(0xF03E3E)
 
-		val data = dbData.groupBy { it.time.toLocalDateTime(timeZone).dayOfYear }
+		val statisticData = dbData.groupBy {
+			val localTime = it.time.toLocalDateTime(timeZone)
 
-		val xData = data.map { it.key }
-		val yData = data.map { it.value }.map { value -> value.map { it.count }.sum() }
+			when (timeChoice) {
+				TimeChoice.HOUR -> localTime.minute
+				TimeChoice.DAY -> localTime.hour
+				TimeChoice.MONTH -> localTime.dayOfYear
+			}
+		}
+
+		val xData = statisticData.map { it.key }
+		val yData = statisticData.map { it.value }.map { value -> value.map { it.count }.sum() }
 
 		val chart = XYChartBuilder()
 			.xAxisTitle(translationsProvider.translate("extensions.statistic.embed.image.dayOfMonth", bundleName = bundle))
@@ -162,6 +187,25 @@ class Statistic : Extension() {
 		val target by optionalMember {
 			name = "user"
 			description = "extensions.statistic.user.arguments.target"
+		}
+		val time by defaultingEnumChoice<TimeChoice> {
+			name = "time"
+			description = "extensions.statistic.arguments.time"
+			defaultValue = TimeChoice.MONTH
+
+			this.typeName = "extensions.statistic.arguments.time.typeName"
+			this.bundle = this@Statistic.bundle
+		}
+	}
+
+	inner class TimeArgs : Arguments() {
+		val time by defaultingEnumChoice<TimeChoice> {
+			name = "time"
+			description = "extensions.statistic.arguments.time"
+			defaultValue = TimeChoice.MONTH
+
+			this.typeName = "extensions.statistic.arguments.time.typeName"
+			this.bundle = this@Statistic.bundle
 		}
 	}
 
